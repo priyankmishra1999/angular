04307ff47da8a6525b68af41627bfdc4dfda6cc4
ja-priyank <ja-priyank@indicchain.com>
2023-01-18 18:39:20 +0530

    UPadted Code

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  title = 'forms';
+
+  regForm: FormGroup;
+
+  simpleForm:FormGroup
+  
+  get alternateEmails() {
+    return this.regForm.get('alternateEmails') as FormArray;
+  }
+
+  addAlternateEmail() {
+    console.log(this.alternateEmails)
+    this.alternateEmails.push(this.fb.control(''));
+    
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+
+  ngOnInit(): void {
+    this.regForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      email: [''],
+      alternateEmails: this.fb.array([])
+      
+      
+    });
+  }
+
+
+
+
+
+
+
+}
---
+++ src/app/sample/server-component/server-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServerComponentComponent } from './server-component.component';
+
+describe('ServerComponentComponent', () => {
+  let component: ServerComponentComponent;
+  let fixture: ComponentFixture<ServerComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServerComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServerComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
