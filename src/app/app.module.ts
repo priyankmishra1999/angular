0b54cb59d110f0c5cbee0c0fbe4691c22b296916
ja-priyank <ja-priyank@indicchain.com>
2023-01-19 18:35:16 +0530

    Updated Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import{ HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SearchFeildComponent } from './search-feild/search-feild.component';
 import { ServerComponentComponent } from './sample/server-component/server-component.component';
 import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFeildComponent,
     ServerComponentComponent,
     ServersComponent,
   ],
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+noNewServer=false;
+serverCreationStatus='online'
+  constructor() { 
+    setTimeout(()=>{
+      this.noNewServer=true;
+    },20000 );
+  }
 
-  constructor() { }
+  ngOnInit(): void { 
+  }
 
-  ngOnInit(): void {
+  oncreateServer(){
+    this.serverCreationStatus='offline'+this.serverName
+  }
+  event:Event
+  serverName:any
+  updateServerName(event){
+    console.log(event.target.value);
+    this.serverName=event.target.value
   }
 
 }
