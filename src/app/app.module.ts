66fd4a05fe072742541fa90dbeca56c969b58c0b
ja-priyank <ja-priyank@indicchain.com>
2023-01-19 13:08:45 +0530

    New Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchFeildComponent } from './search-feild/search-feild.component';
 import { ServerComponentComponent } from './sample/server-component/server-component.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFeildComponent,
     ServerComponentComponent,
+    ServersComponent,
   ],
   imports: [
     BrowserModule,
